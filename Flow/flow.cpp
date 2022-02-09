c59caa8d5720403f6f478c696b318d4c7abd9c41
wa <wa@wa-MS-7640>
2022-02-09 00:04:34 -0500

    linux windows merge

---
+++ Flow/flow.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+using namespace sf;
+
+SoundBuffer bufferSample(string sample)
+{
+	SoundBuffer buffer;
+	if(!buffer.loadFromFile("../Sounds/"+sample))
+		return buffer;
+	return buffer;
+}
+
+Sound loadSample(SoundBuffer& buffer)
+{
+	Sound sample;
+	sample.setBuffer(buffer);
+	sample.setVolume(5.f);
+	return sample;
+}
+
+int main()
+{
+	srand(int(time(0)));
+
+	SoundBuffer buffer;
+	buffer = bufferSample("Sine_wave_440.ogg");
+	Sound sample1;
+	sample1 = loadSample(buffer);
+
+	SoundBuffer buffer2;
+	buffer2 = bufferSample("kick-hip-hop-punchy-3.wav");
+	Sound sample2;
+	sample2 = loadSample(buffer2);
+
+	int waitTime = rand()% 500000 + 50000;
+
+	int wait;
+	for(;;)
+	{
+		wait = rand() % 12;
+		sample1.stop();
+		sample2.stop();
+		float pitch = 0.0f + float(wait/3);
+		sample1.setPitch(pitch);
+		sample1.play();
+		sample2.play();
+		usleep(waitTime);
+	}
+
+    return 0;
+}

2e2d750e4f82cc4507aa07337dda55b3a60a836e
wa <wa@wa-MS-7640>
2022-02-11 03:34:50 -0500

    started controler for 32 notes per beat for the kick drum

--- Flow/flow.cpp
+++ Flow/flow.cpp
@@ -58,6 +58,24 @@ time_t getMilliseconds()
 
 const float secsmins = 60.0f;
 
+void playSample2(time_t startTime, int beatTime, int& sample2NextStep, time_t currentTime, float bpm, int bpmCmp, Sound& sample2, array<int,32> sample2Bar,  int& sample2BarPlayed)
+{
+	if(sample2Bar[sample2BarPlayed] == 1)
+	{
+		cout << "*****Sample 2*****" << endl;
+		cout << startTime << endl << beatTime << endl;
+		cout << "supposed to be: " << ((startTime * 1000) + ((bpmCmp + 1) * beatTime)) << endl;
+		cout << "really is: " << "\t" << currentTime << endl;
+		cout << "difference: " << (((startTime * 1000) + ((bpmCmp + 1) * beatTime)) - currentTime) << endl;
+		cout << "bpm: " << bpm << ", bar: " << (bpmCmp + 1) << endl;
+		sample2.stop();
+		sample2.play();
+	}
+	++sample2BarPlayed;
+	sample2BarPlayed = sample2BarPlayed >= 32 ? 0 : sample2BarPlayed;
+	sample2NextStep += (beatTime - (32 - sample2BarPlayed));
+}
+
 int main()
 {
 	time_t startTime = getMilliseconds();
@@ -72,32 +90,52 @@ int main()
 	buffer2 = bufferSample("kick-hip-hop-punchy-3.wav");
 	Sound sample2;
 	sample2 = loadSample(buffer2);
+	int sample2NextStep = 0;
+
+	//test array for 1 bar of sample2 (kick drum)
+	array<int,32> sample2Bar = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int sample2BarPlayed = 0;
 
 	float bpm = 120.0f;
-	int waitTime = int((secsmins / bpm) * 1000000.f);
+	int beatTime = int((secsmins / bpm) * 1000000.f);
 
 	int wait;
-	int cmp = 0;
+	int bpmCmp = 0;
 	for(;;)
 	{
 		time_t currentTime = getMilliseconds() * 1000;
-		time_t shouldBe = ((startTime * 1000) + (waitTime * cmp));
-		if(shouldBe <= currentTime)
+		time_t bpmNextBar = time_t(((startTime * 1000) + (beatTime * (bpmCmp + 1))));
+		sample2NextStep = sample2NextStep == 0 ? int(bpmNextBar) : sample2NextStep;
+//		if(shouldBe <= currentTime)
+//		{
+//			clear_screen();
+//			cout << startTime << endl << waitTime << endl;
+//			cout << "supposed to be: " << shouldBe << endl;
+//			cout << "really is: " << "\t" << currentTime << endl;
+//			cout << "difference: " << (shouldBe - currentTime) << endl;
+//			cout << "bpm: " << bpm << endl;
+//			wait = rand() % 12;
+//			sample1.stop();
+//			sample2.stop();
+//			float pitch = 0.0f + float(wait/3);
+//			sample1.setPitch(pitch);
+//			sample1.play();
+//			sample2.play();
+//			++bpmCmp;
+//		}
+		if(sample2NextStep <= currentTime)
 		{
 			clear_screen();
-			cout << startTime << endl << waitTime << endl;
-			cout << "supposed to be: " << shouldBe << endl;
-			cout << "really is: " << "\t" << currentTime << endl;
-			cout << "difference: " << (shouldBe - currentTime) << endl;
-			cout << "bpm: " << bpm << endl;
-			wait = rand() % 12;
-			sample1.stop();
-			sample2.stop();
-			float pitch = 0.0f + float(wait/3);
-			sample1.setPitch(pitch);
-			sample1.play();
-			sample2.play();
-			++cmp;
+			playSample2(startTime, beatTime, sample2NextStep, currentTime, bpm, bpmCmp, sample2, sample2Bar, sample2BarPlayed);
+		}
+		if(bpmNextBar <= currentTime)
+		{
+			++bpmCmp;
+			if(bpmCmp >= 120)
+			{
+				cout << "120 beats: " << (currentTime - (startTime * 1000)) << endl;
+				cin >> wait;
+			}
 		}
 	}
 

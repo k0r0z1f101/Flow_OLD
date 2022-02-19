8d4db28926da8a7169625453344de1c81bb9a5ed
wa <wa@wa-MS-7640>
2022-02-18 22:27:36 -0500

    migration vers classe terminée, nettoyage

--- Flow/flow.cpp
+++ Flow/flow.cpp
@@ -49,10 +49,11 @@ public:
 	}
 };
 
+//controleur des différents sons, buffers et samples
 class Sample
 {
 private:
-	time_t nextStep = 0;
+	time_t sampleNextStep = 0;
 	int sampleBarsPlayed = 0;
 	string sampleBehaviour = "default";
 	array<int, barSize> sampleBar = { 1, 0, 0, 0 };
@@ -75,6 +76,51 @@ public:
 	{
 		return sample;
 	}
+	int getSampleBarsPlayed()
+	{
+		return sampleBarsPlayed;
+	}
+	time_t getNextStep()
+	{
+		return sampleNextStep;
+	}
+	void writeNextStep(time_t newStep)
+	{
+		sampleNextStep = newStep;
+	}
+	void playSample(int beatTime, bool pitch = false, float volume = 5.f)
+	{
+		string sampleBeat;
+		for(int i = 0; i < barSize; ++i)
+		{
+			sampleBeat += to_string(sampleBar[i]) + " ";
+		}
+		cout << "*****Sample***** " << sampleBeat << endl;
+		cout << "step: " << sampleBarsPlayed << endl;
+		cout << "sampleNextStep: " << sampleNextStep << endl;
+		if(sampleBar[sampleBarsPlayed] == 1)
+		{
+			if(pitch) sample.setPitch(0.5f + float((rand() % 501)/100));
+			sample.setVolume(volume);
+			sample.play();
+		}
+		++sampleBarsPlayed;
+		sampleBarsPlayed = sampleBarsPlayed >= barSize ? 0 : sampleBarsPlayed;
+		sampleNextStep += (beatTime / barSize);
+	}
+	void buildSampleBar(string behaviour = "default")
+	{
+		if(behaviour == "default")
+		{
+			int notesLeft = 2;
+			for(int i = 0; i < barSize; ++i)
+			{
+				sampleBar[i] = notesLeft > 0 ? rand() % 2 : rand() % 2;
+				sampleBar[i] = notesLeft == 2 && i == (barSize - 1) ? 1 : sampleBar[i];
+				notesLeft = sampleBar[i] != 0 ? notesLeft - 1 : notesLeft;
+			}
+		}
+	}
 };
 
 int main()
@@ -84,38 +130,23 @@ int main()
 
 	//sample variables //0: sine wave, 1: kick drum
 	const size_t sampleNumbers = 3;
-	//array<SoundBuffer, sampleNumbers> buffers = { bufferSample("Sine_wave_440.ogg"), bufferSample("kick-hip-hop-punchy-3.wav") };
-	array<SoundBuffer, sampleNumbers> buffers = { bufferSample("ahh-choir-female-vocal-high_C_minor.wav"), bufferSample("juicy-kick-power-hit.wav"), bufferSample("kick-hip-hop-punchy-3.wav") };
-	array<Sound, sampleNumbers> samples = { loadSample(buffers[0]), loadSample(buffers[1]), loadSample(buffers[2]) };
-	array<time_t, sampleNumbers> nextSteps = { 0, 0, 0 };
-	//array of array for sampleBars
-	array<int, sampleNumbers> sampleBarsPlayed = { 0, 0, 0 };
-	array<string,sampleNumbers> sampleBehaviours = { "default", "default", "default" };
-	vector<array<int, barSize>> sampleBars = { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } }; //test
-
-	array<int,barSize> sample1Bar = { 1, 0, 0, 0 };
-
-	array<int,barSize> sample2Bar = { 1, 0, 0, 0 };
-
-	array<int,barSize> sample3Bar = { 1, 0, 0, 0 };
 
 	//initialiser les samples et les buffers
 	Sample sineWave;
 	Sample kickDrum1;
 	Sample kickDrum2;
 	array<Sample, sampleNumbers> samplesObject = { sineWave, kickDrum1, kickDrum2 };
-
 	array<string, sampleNumbers> sampleNames = { "ahh-choir-female-vocal-high_C_minor.wav", "juicy-kick-power-hit.wav", "kick-hip-hop-punchy-3.wav" };
-	for(int i = 0; i < sampleNumbers; ++i)
+
+	for(size_t i = 0; i < sampleNumbers; ++i)
 	{
 		samplesObject[i].initBuffer(sampleNames[i]);
 		samplesObject[i].initSound();
 	}
 
+	int wait;
 	float bpm = 60.0f;
 	int beatTime = int((secsmins / bpm) * 1000000.f);
-
-	int wait;
 	int barCmp = 0;
 	int measureCmp = 0;
 	time_t bpmNextMeasure;
@@ -148,75 +179,51 @@ int main()
 		shape.setFillColor(sf::Color(100, 250, 50));
 		shape.setPosition(windowWidth / 2 - talkerRadius, windowHeight - talkerRadius * 2);
 
-		//
-		//		for(;;)
-		//		{
-				time_t currentTime = getMilliseconds() * 1000;
-				time_t bpmNextBar = time_t(((startTime * 1000) + (beatTime * (barCmp + 1))));
-				bpmNextMeasure = barCmp % measureSize == 0 ? time_t(((startTime * 1000) + ((beatTime * measureSize) * (measureCmp + 1)))) : bpmNextMeasure;
+		time_t currentTime = getMilliseconds() * 1000;
+		time_t bpmNextBar = time_t(((startTime * 1000) + (beatTime * (barCmp + 1))));
+		bpmNextMeasure = barCmp % measureSize == 0 ? time_t(((startTime * 1000) + ((beatTime * measureSize) * (measureCmp + 1)))) : bpmNextMeasure;
 
-				//nettoyer l'écran si un sample joue
-				if(time_t(nextSteps[0]) <= currentTime || time_t(nextSteps[1]) <= currentTime || time_t(nextSteps[2]) <= currentTime)
-				{
-					clear_screen();
-					cout << "started: \t" << startTime << ", bar time: " << beatTime << endl;
-					cout << "current time: " << "\t" << currentTime << endl;
-					cout << "bpm: " << bpm << ", measure: " << (measureCmp + 1) << ", bar: " << (barCmp + 1) << endl;
-				}
+		//nettoyer l'écran si un sample joue
+		if(time_t(samplesObject[0].getNextStep()) <= currentTime || time_t(samplesObject[1].getNextStep()) <= currentTime || time_t(samplesObject[2].getNextStep()) <= currentTime)
+		{
+			clear_screen();
+			cout << "started: \t" << startTime << ", bar time: " << beatTime << endl;
+			cout << "current time: " << "\t" << currentTime << endl;
+			cout << "bpm: " << bpm << ", measure: " << (measureCmp + 1) << ", bar: " << (barCmp + 1) << endl;
+		}
 
-				//jouer les samples
+		//jouer les samples
+		for(size_t i = 0; i < sampleNumbers; ++i)
+		{
+			//jouer le sample si c'est son temps
+			samplesObject[i].writeNextStep(samplesObject[i].getNextStep() == 0 ? bpmNextBar : samplesObject[i].getNextStep());
+			if(time_t(samplesObject[i].getNextStep()) <= currentTime)
+			{
+				samplesObject[i].playSample(beatTime, true);
+			}
+		}
 
-				//jouer le sample 1 si c'est son temps
-				nextSteps[0] = nextSteps[0] == 0 ? bpmNextBar : nextSteps[0];
-				if(time_t(nextSteps[0]) <= currentTime)
-				{
-//					playSample(startTime, beatTime, nextSteps[0], currentTime, bpm, measureCmp, barCmp, samples[0], sample1Bar, sampleBarsPlayed[0], shape, true, true);
-					playSample(startTime, beatTime, nextSteps[0], currentTime, bpm, measureCmp, barCmp, samplesObject[0].sample, sample1Bar, sampleBarsPlayed[0], shape, true, true);
-				}
+		//compteur de bar
+		if(bpmNextBar <= currentTime)
+		{
+			samplesObject[0].buildSampleBar();
+			++barCmp;
+			if(barCmp >= 120)
+			{
+				cout << "120 bars: " << (currentTime - (startTime * 1000)) << endl;
+				cin >> wait;
+			}
+		}
 
-//				//jouer le sample 2 si c'est son temps
-				nextSteps[1] = nextSteps[1] == 0 ? bpmNextBar : nextSteps[1];
-				if(time_t(nextSteps[1]) <= currentTime)
-				{
-//					playSample(startTime, beatTime, nextSteps[1], currentTime, bpm, measureCmp, barCmp, samples[1], sample2Bar, sampleBarsPlayed[1], shape, false, false, 10.f);
-					playSample(startTime, beatTime, nextSteps[1], currentTime, bpm, measureCmp, barCmp, samplesObject[1].sample, sample2Bar, sampleBarsPlayed[1], shape, true, true);
-				}
-//
-//				//jouer le sample 3 si c'est son temps
-				nextSteps[2] = nextSteps[2] == 0 ? bpmNextBar : nextSteps[2];
-				if(time_t(nextSteps[2]) <= currentTime)
-				{
-					playSample(startTime, beatTime, nextSteps[2], currentTime, bpm, measureCmp, barCmp, samples[2], sample3Bar, sampleBarsPlayed[2], shape, false, false, 8.f);
-					playSample(startTime, beatTime, nextSteps[2], currentTime, bpm, measureCmp, barCmp, samplesObject[2].sample, sample3Bar, sampleBarsPlayed[2], shape, true, true);
-				}
+		//compteur de mesures et reconstruction des samples bar
+		if(bpmNextMeasure <= currentTime)
+		{
+			samplesObject[1].buildSampleBar();
+			samplesObject[2].buildSampleBar();
+			++measureCmp;
+		}
 
-				//compteur de bar
-				if(bpmNextBar <= currentTime)
-				{
-					sampleBehaviour(sample1Bar);
-		//			sampleBehaviour(sample2Bar);
-					++barCmp;
-					if(barCmp >= 120)
-					{
-						cout << "120 bars: " << (currentTime - (startTime * 1000)) << endl;
-						cin >> wait;
-					}
-				}
-
-				//compteur de mesures et reconstruction des samples bar
-				if(bpmNextMeasure <= currentTime)
-				{
-		//			sampleBehaviour(sample1Bar);
-					sampleBehaviour(sample2Bar);
-					sampleBehaviour(sample3Bar);
-					++measureCmp;
-				}
-				//waitKeyToPress();
-
-		//	}
-
-
-		if(sampleBarsPlayed[1] == 1)
+		if(samplesObject[0].getSampleBarsPlayed() == 1)
 		{
 			shape.setScale(1.2f,1.2f);
 			shape.setPosition(windowWidth / 2 - talkerRadius * 1.2, windowHeight - talkerRadius * 1.2 * 2);
@@ -225,11 +232,6 @@ int main()
 
 		// end the current frame
 		talkieWindow.display();
-
-
-
 	}
-
-
     return 0;
 }
